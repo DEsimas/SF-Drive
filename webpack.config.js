e64b48bdc648ea5803cd146af2ecded3bd1bf745
DEsimas <simasuh@gmail.com>
2021-01-23 18:28:07 +0300

    second html

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const {CleanWebpackPlugin} = require('clean-webpack-plugin')
 
 module.exports = {
     mode: 'development',
@@ -13,7 +14,15 @@ module.exports = {
     },
     plugins: [
         new HtmlWebpackPlugin({
+            filename: 'FAQ.html',
             template: './src/FAQ.html'
-        })
+        }),
+
+        new HtmlWebpackPlugin({
+            filename: 'aboutUs.html',
+            template: './src/aboutUs.html'
+        }),
+
+        new CleanWebpackPlugin()
     ]
 }
