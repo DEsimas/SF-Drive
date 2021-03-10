ae688f68026a6f00156af5b56b73192f541b75ad
DEsimas <simasuh@gmail.com>
2021-03-10 18:39:33 +0300

    somehow connected with server

--- src/components/requests.js
+++ src/components/requests.js
@@ -1,9 +1,10 @@
 import { writeTokens, ACCESS_TOKEN_STORAGE_KEY } from "./JWT";
 
-const SERVER_ADDRESS = "http://localhost:8000/";
+const SERVER_ADDRESS = "http://localhost:3000/registration/";
+let USER_ID_KEY = "USER_ID";
 
 export const registrationRequest = data => {
-    fetch(SERVER_ADDRESS, 
+    fetch(SERVER_ADDRESS,
         {
             method: "POST",
             body: JSON.stringify(data),
@@ -11,24 +12,29 @@ export const registrationRequest = data => {
                 'Content-Type': 'application/json;charset=utf-8'
             }
         })
-            .then(res => res.json())
-            .then(data => {
-                console.log(data);
-                if(data.accessToken) writeTokens(data.accessToken, data.refreshToken);
-                alert('success');
-            })
-            .catch(e => document.getElementById('alert').classList.remove('hide'));
+        .then(res => res.json())
+        .then(data => {
+            console.log(data);
+            localStorage.setItem(USER_ID_KEY, data.id);
+            console.log(USER_ID)
+            // if(data.accessToken) writeTokens(data.accessToken, data.refreshToken);
+            alert('success');
+        })
+        .catch(e => document.getElementById('alert').classList.remove('hide'));
 };
 
 export const authorizationRequest = data => {
+
+
+
     fetch(SERVER_ADDRESS + 'authorization/',
-    {
-        method: "POST",
-        body: JSON.stringify(data),
-        headers: {
-            'Content-Type': 'application/json;charset=utf-8'
-        }
-    })
+        {
+            method: "POST",
+            body: JSON.stringify(data),
+            headers: {
+                'Content-Type': 'application/json;charset=utf-8'
+            }
+        })
         .then(res => res.json())
         .then(data => {
             console.log(data);
@@ -39,20 +45,37 @@ export const authorizationRequest = data => {
 };
 
 export const recoveryRequest = data => {
+    let userID = localStorage.getItem(USER_ID_KEY);
 
-    fetch(SERVER_ADDRESS + 'recovery/',
-    {
-        method: "POST",
-        body: JSON.stringify(data),
-        headers: {
-            'Content-Type': 'application/json;charset=utf-8',
-            'Authorization': `Bearer ${localStorage.getItem(ACCESS_TOKEN_STORAGE_KEY)}`
-        }
-    })
+    console.log(userID);
+
+    let isCorrect = true;
+
+    fetch(SERVER_ADDRESS + userID + '/')
         .then(res => res.json())
-        .then(data => {
-            if(data.message == "ok") alert('success');
-            else alert(data.message);
-        })
-        .catch(e => console.log(e));
+        .then(res => {
+            if (res.email != data.email) {
+                alert("Неверная почта");
+                isCorrect = false;
+            };
+
+            if (!isCorrect) return 1;
+
+            fetch(SERVER_ADDRESS + userID + '/',
+                {
+                    method: "PUT",
+                    body: JSON.stringify(data),
+                    headers: {
+                        'Content-Type': 'application/json;charset=utf-8',
+                    }
+                })
+                .then(res => res.json())
+                .then(data => {
+                    if (data.message != "user not found") alert('success');
+                    else alert(data.message);
+                })
+                .catch(e => console.log(e));
+        });
+
+
 };
